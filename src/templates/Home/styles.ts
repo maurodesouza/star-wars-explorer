2fa930baac6f6703b63c73077d5ab58e4dc55af2
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-30 20:35:42 -0300

    ✨  feat: add home template

---
+++ src/templates/Home/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Results = styled.div`
+  flex: 1;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Wrapper = styled.div`
+  height: 30rem;
+`;
