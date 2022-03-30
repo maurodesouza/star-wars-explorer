55cbbfe903a06c04f3cf3fa1a689bfa64da63a3b
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-30 20:28:45 -0300

    🐛  fix: next/router on storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ module.exports = {
   stories: [
     '../src/components/**/stories.@(js|jsx|ts|tsx)'
   ],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials', 'storybook-addon-next-router'],
 
   webpackFinal: config => {
     config.resolve.modules.push(`${process.cwd()}/src`);
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,14 @@
 import './next-image.mock'
 
 import { ThemeProvider } from 'styled-components';
+import { RouterContext } from "next/dist/shared/lib/router-context";
 import { GlobalStyles, theme } from 'styles';
 
-import GlobalStyles from 'styles/global';
-import theme from 'styles/theme/default';
-
+export const parameters = {
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+}
 
 export const decorators = [
   (Story) => (
