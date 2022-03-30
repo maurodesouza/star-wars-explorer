4ee61936e6a94d674f0675a7dd1ff7906f0c82de
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-30 20:34:39 -0300

    ✨  feat: add use search hook

---
+++ src/hooks/use-search/index.ts
@@ -0,0 +1,106 @@
+import { events } from 'app';
+import axios from 'axios';
+import { useEffect, useRef, useState } from 'react';
+import { Entities, Events } from 'types';
+
+type Cache<T = any> = {
+  [key: string]: T;
+};
+
+type MakeSearch = {
+  entity: Entities;
+  search: string;
+};
+
+type FetchDataArgs = CustomEvent<MakeSearch>;
+
+const useSearch = () => {
+  const cacheRef = useRef<Cache>({});
+  const abortRef = useRef<AbortController>();
+
+  const entityRef = useRef<Entities>(Entities.CHARACTERS);
+  const searchRef = useRef('');
+
+  const [data, setData] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const fetchData = async (event: FetchDataArgs) => {
+    const saveEntity = entityRef.current;
+    const saveSearch = searchRef.current;
+
+    const { entity = saveEntity, search = saveSearch } = event.detail;
+
+    entityRef.current = entity;
+    searchRef.current = search;
+
+    if (!search) return;
+
+    abortRef.current?.abort();
+
+    const cache = cacheRef.current;
+    const key = `${entity}-${search}`;
+
+    const result = cache[key];
+
+    if (result) {
+      setData(result);
+      return;
+    }
+
+    try {
+      setIsFetching(true);
+
+      const abortController = new AbortController();
+
+      abortRef.current = abortController;
+
+      const { data } = await axios.get('/api/data', {
+        params: { entity, search },
+        signal: abortController.signal,
+      });
+
+      cacheRef.current[key] = data;
+
+      setData(data);
+    } catch (err) {
+      console.error(err);
+
+      const wasCanceled =
+        err instanceof Object && err.constructor.name === 'Cancel';
+
+      if (wasCanceled) return;
+
+      const defaultErrorMessage =
+        'An error has occurred, try again or come back soon';
+
+      const error = axios.isAxiosError(err)
+        ? err.response?.data.error
+        : defaultErrorMessage;
+
+      setData([]);
+      events.search.error(error);
+    } finally {
+      setIsFetching(false);
+
+      abortRef.current = undefined;
+    }
+  };
+
+  useEffect(() => {
+    events.on(Events.MAKE_SEARCH, fetchData);
+
+    return () => {
+      events.off(Events.MAKE_SEARCH, fetchData);
+    };
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      abortRef.current?.abort();
+    };
+  }, []);
+
+  return { data, isFetching };
+};
+
+export { useSearch };
