09feeba25caaa438fadfcbe9d9ec534bced6cd33
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-30 20:29:14 -0300

    ⚡  chore: improve storybook preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,18 +2,36 @@ import './next-image.mock'
 
 import { ThemeProvider } from 'styled-components';
 import { RouterContext } from "next/dist/shared/lib/router-context";
+
 import { GlobalStyles, theme } from 'styles';
+import { Gap } from 'components';
 
 export const parameters = {
   nextRouter: {
     Provider: RouterContext.Provider,
   },
+
+  layout: 'centered',
+
+  backgrounds: {
+    default: 'default',
+    values: [
+      {
+        name: 'default',
+        value: theme.colors.bg
+      },
+    ]
+  }
 }
 
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
-      <Story />
+        <div style={{ height: '100%', paddingBlock: theme.grid.gutter }}>
+          <Gap>
+            <Story />
+          </Gap>
+        </div>
       <GlobalStyles />
     </ThemeProvider>
   ),
