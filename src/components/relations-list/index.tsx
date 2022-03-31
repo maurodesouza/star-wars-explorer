1041ea8f4f732b72426e1a2d7e220459456865ba
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-31 18:53:45 -0300

    ⚡  chore: show items count

--- src/components/relations-list/index.tsx
+++ src/components/relations-list/index.tsx
@@ -1,12 +1,13 @@
-import { CardList } from 'components/card-list';
+import { CardList, CardListProps } from 'components/card-list';
 import { Entities, Entity } from 'types';
+
 import * as S from './styles';
 
-type RelationsListProps = {
-  relations: Entity[];
+export type RelationsListProps = {
+  relations: CardListProps['items'];
 };
 
-const RelationsList = ({ relations }: RelationsListProps) => {
+const RelationsList = ({ relations = [] }: RelationsListProps) => {
   const entities = relations.reduce((obj, item) => {
     const key = item.entity;
     const entities = obj[key] || [];
@@ -14,16 +15,28 @@ const RelationsList = ({ relations }: RelationsListProps) => {
     obj[key] = [...entities, item];
 
     return obj;
-  }, {} as Record<Entities, Entity[]>);
+  }, {} as Record<Entities, (Entity & { empty?: boolean })[]>);
 
   return (
     <S.Container>
-      {Object.entries(entities).map(([key, items]) => (
-        <S.Wrapper key={key}>
-          <S.Label>{key}</S.Label>
-          <CardList items={items} />
-        </S.Wrapper>
-      ))}
+      {Object.entries(entities).map(([key, items]) => {
+        const total = items.length;
+        const emptyCount = items.filter(item => !!item.empty).length;
+
+        const hasEmpty = !!emptyCount;
+        const finded = items.length - emptyCount;
+
+        const label = `${key} (${
+          hasEmpty && finded ? `${finded}/` : ''
+        }${total})`;
+
+        return (
+          <S.Wrapper key={key}>
+            <S.Label>{label}</S.Label>
+            <CardList items={items} />
+          </S.Wrapper>
+        );
+      })}
     </S.Container>
   );
 };
