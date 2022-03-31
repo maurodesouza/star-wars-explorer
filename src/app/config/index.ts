676b6ab4756113f12e9ef11a59ee6c7bf437d438
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-31 18:45:43 -0300

    ⚡  chore: add entites count config

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,7 +1,34 @@
+import { Entities } from 'types';
 import { envs } from './envs';
 
 const config = {
   envs,
+
+  entities: {
+    [Entities.CHARACTERS]: {
+      count: 82,
+    },
+
+    [Entities.FILMS]: {
+      count: 6,
+    },
+
+    [Entities.PLANETS]: {
+      count: 82,
+    },
+
+    [Entities.SPECIES]: {
+      count: 37,
+    },
+
+    [Entities.STARSHIPS]: {
+      count: 36,
+    },
+
+    [Entities.VEHICLES]: {
+      count: 60,
+    },
+  },
 };
 
 export { config };
