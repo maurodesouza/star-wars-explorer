05fc595ff229aac690511d027d6eec16fa607fa1
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-31 11:07:25 -0300

    ⚡  chore: add 404 lottie animation

--- src/components/animation/animations.ts
+++ src/components/animation/animations.ts
@@ -1,9 +1,11 @@
 import loadingAnimation from 'assets/lottie/loading.json';
 import noResultAnimation from 'assets/lottie/no-result.json';
+import animation404 from 'assets/lottie/404.json';
 
 const animations = {
   loading: loadingAnimation,
   'no-result': noResultAnimation,
+  '404': animation404,
 };
 
 export { animations };
