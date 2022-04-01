b9b11860dde9a3a12e099c883721d085c626dbb7
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-31 22:55:05 -0300

    ⚡  chore: improve favorite handle stories

--- src/components/favorite-handle/stories.tsx
+++ src/components/favorite-handle/stories.tsx
@@ -3,8 +3,17 @@ import { Meta, Story } from '@storybook/react/types-6-0';
 import { FavoriteHandle, FavoriteHandleProps } from '.';
 
 export default {
-  title: 'FavoriteHandle',
+  title: 'Entity/Favorite button',
   component: FavoriteHandle,
+
+  argTypes: {
+    id: { control: { disable: true } },
+    title: { control: { disable: true } },
+    entity: { control: { disable: true } },
+    image: { control: { disable: true } },
+    extras: { table: { disable: true } },
+    relations: { table: { disable: true } },
+  },
 } as Meta<FavoriteHandleProps>;
 
 export const Basic: Story<FavoriteHandleProps> = args => (
