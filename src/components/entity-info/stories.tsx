e5bf636e3580fa04d7afdf9b08985275164ed2d1
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-31 22:56:10 -0300

    ⚡  chore: improve entity info stories

--- src/components/entity-info/stories.tsx
+++ src/components/entity-info/stories.tsx
@@ -1,10 +1,32 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
 
+import { mock as relationsMock } from 'components/relations-list/mock';
+import { mock as detaisMock } from 'components/entity-details/mock';
+
 import { EntityInfo, EntityInfoProps } from '.';
 
 export default {
-  title: 'EntityInfo',
+  title: 'Entity/Info',
   component: EntityInfo,
+
+  parameters: {
+    layout: 'fullscreen',
+  },
+
+  argTypes: {
+    id: { control: { disable: true } },
+    title: { control: { disable: true } },
+    entity: { control: { disable: true } },
+  },
+
+  args: {
+    ...relationsMock,
+    ...detaisMock,
+  },
 } as Meta<EntityInfoProps>;
 
-export const Basic: Story<EntityInfoProps> = args => <EntityInfo {...args} />;
+export const Basic: Story<EntityInfoProps> = args => (
+  <div style={{ width: '100%', maxWidth: '840px', marginInline: 'auto' }}>
+    <EntityInfo {...args} />
+  </div>
+);
