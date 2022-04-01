f9a5d062b324b58dde7091efabb7be925abb595e
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-04-01 00:08:42 -0300

    ⚡  chore: improve card usability on mobile

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,9 +1,12 @@
+import { TouchEvent, useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 
 import { Image, FavoriteHandle } from 'components';
 import { slugfy } from 'utils';
 
+import { events } from 'app';
 import { Entities, Entity, Partials } from 'types';
+
 import * as S from './styles';
 
 export type CardProps = Omit<
@@ -13,18 +16,69 @@ export type CardProps = Omit<
 
 const Card = (data: CardProps) => {
   const { entity = Entities.CHARACTERS, image, title } = data;
+
+  const containerRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
 
-  const handleRedirect = async (e: React.MouseEvent) => {
-    e.preventDefault();
+  const [isTouchscreen, setIsTouchscreen] = useState(false);
 
-    router.push(href);
+  const [isDragging, setIsDragging] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleRedirect = () => router.push(href);
+
+  const onMouseUp = () => {
+    if (isTouchscreen) {
+      return;
+    }
+
+    handleRedirect();
   };
+
+  const onTouch = () => {
+    if (isDragging) {
+      setIsDragging(false);
+      setIsFocused(false);
+
+      return;
+    }
+
+    if (isFocused) {
+      events.off('touchend', onTouchEnd);
+      handleRedirect();
+
+      return;
+    }
+
+    setIsFocused(true);
+    events.on('touchend', onTouchEnd);
+  };
+
+  const onTouchEnd = (e: TouchEvent) => {
+    if (containerRef.current!.contains(e.target as Node)) return;
+
+    handleRemoveFocused();
+  };
+
+  const handleRemoveFocused = () => {
+    setIsFocused(false);
+    events.off('touchend', onTouchEnd);
+  };
+
+  useEffect(() => {
+    setIsTouchscreen(!!window.matchMedia('(any-pointer: coarse)').matches);
+  }, []);
 
   const href = `/universe/${entity}/${slugfy(title)}`;
 
   return (
-    <S.Container role="link" onClick={handleRedirect}>
+    <S.Container
+      ref={containerRef}
+      role="link"
+      onMouseUp={onMouseUp}
+      onTouchEnd={onTouch}
+      onTouchMove={() => !isDragging && setIsDragging(true)}
+    >
       <Image image={image} alt={title} />
 
       <S.Content>
